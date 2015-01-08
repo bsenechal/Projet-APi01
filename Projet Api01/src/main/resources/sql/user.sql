184339f0c453e105d75d1864ebf2884ad0d4de98
bsenechal <benoit.senechal1993@gmail.com>
2015-01-08 22:25:16 +0100

    Même commit

---
+++ Projet Api01/src/main/resources/sql/user.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE `projet_api01` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(60) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telephone` int(11) NOT NULL,
+  `creation_date` date DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
