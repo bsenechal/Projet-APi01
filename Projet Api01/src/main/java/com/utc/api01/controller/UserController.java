4a873e9f9658c926bb8158d766817f59f9f46325
adobeSlash <stephane.karagulmez@hotmail.fr>
2015-01-11 16:44:07 +0100

    Corrige des erreurs au lancement de tomcat concernant l'utilisation des Generiques

--- Projet Api01/src/main/java/com/utc/api01/controller/UserController.java
+++ Projet Api01/src/main/java/com/utc/api01/controller/UserController.java
@@ -13,16 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.utc.api01.model.User;
-import com.utc.api01.service.UserService;
+import com.utc.api01.service.GeneriqueService;
 
 @Controller
 public class UserController {
 	
-	private UserService userService;
+	private GeneriqueService<User> userService;
 	
 	@Autowired(required=true)
 	@Qualifier(value="userService")
-	public void setUserService(UserService us){
+	public void setUserService(GeneriqueService<User> us){
 		this.userService = us;
 	}
 	
@@ -30,14 +30,14 @@ public class UserController {
 	@RequestMapping(value = "/admin/users", method = RequestMethod.GET)
 	public String listUsers(Model model) {
 		model.addAttribute("user", new User());
-		model.addAttribute("listUsers", this.userService.listUsers());
+		model.addAttribute("listUsers", this.userService.list());
 		return "user";
 	}
 	
 	@RequestMapping(value= "/admin/user/add", method = RequestMethod.POST)
 	public String addUser(@ModelAttribute("user") User u){
 		u.setCreationDate(new SimpleDateFormat("YYYY-MM-DD").format(new Date()));
-		this.userService.addUser(u);
+		this.userService.add(u);
 		
 		return "redirect:/admin/users";
 	}
@@ -45,19 +45,19 @@ public class UserController {
 	@RequestMapping("/admin/remove/{idUser}")
     public String removeUser(@PathVariable("idUser") int id){
 		
-        this.userService.removeUser(id);
+        this.userService.remove(id);
         return "redirect:/admin/users";
     }
  
     @RequestMapping("/admin/edit/{idUser}")
     public String editUser(@PathVariable("idUser") int idUser, Model model){
-		model.addAttribute("user", this.userService.getUserById(idUser));
+		model.addAttribute("user", this.userService.getById(idUser));
         return "editUser";
     }
 	
     @RequestMapping(value= "/admin/saveEdit", method = RequestMethod.POST)
     public String editAndSaveUser(@ModelAttribute("user") User u){
-    	this.userService.updateUser(u);
+    	this.userService.update(u);
         return "redirect:/admin/users";
     }
 }
--- Projet Api01/src/main/java/com/utc/api01/service/UserServiceImpl.java
+++ Projet Api01/src/main/java/com/utc/api01/service/UserServiceImpl.java
@@ -5,42 +5,41 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.utc.api01.dao.UserDao;
+import com.utc.api01.dao.GeneriqueDao;
 import com.utc.api01.model.User;
-import com.utc.api01.service.UserService;
 
 @Service
 public class UserServiceImpl implements GeneriqueService<User>{
 	
-	private UserDao userDao;
+	private GeneriqueDao<User> userDao;
 
-	public void setUserDao(UserDao userDao) {
+	public void setUserDao(GeneriqueDao<User> userDao) {
 		this.userDao = userDao;
 	}
 
 	@Transactional
 	public void addUser(User u) {
-		this.userDao.addUser(u);
+		this.userDao.add(u);
 	}
 
 	@Transactional
 	public void updateUser(User u) {
-		this.userDao.updateUser(u);
+		this.userDao.update(u);
 	}
 
 	@Transactional
 	public List<User> listUsers() {
-		return this.userDao.listUsers();
+		return this.userDao.list();
 	}
 
 	@Transactional
 	public User getUserById(int id) {
-		return this.userDao.getUserById(id);
+		return this.userDao.getById(id);
 	}
 
 	@Transactional
 	public void removeUser(int id) {
-		this.userDao.removeUser(id);
+		this.userDao.remove(id);
 	}
 
 	@Override
